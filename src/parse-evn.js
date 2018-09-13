fb4e3c9ed55fdea52b1ec68cfe8a2784a0df1b87
吴如富 <j.code@qq.com>
2018-09-13 11:03:27 +0800

    添加配置文件变化监听，在文件变化后重新解析配置

--- src/parse-evn.js
+++ src/parse-evn.js
@@ -7,28 +7,38 @@ let evn;
  * @return {*}
  */
 function parseEvn (options) {
-  if (evn) {
-    return evn;
-  }
-
-  const chartSet = options.chartSet || 'utf-8';
-  const evnFile = options.filename || '.preprocess-evn';
-  const content = fs.readFileSync(evnFile, chartSet).toString();
-
-  evn = {};
-  content.split(/[\r\n]+/g).map(line => {
-    line = line.trim();
-
-    // 以#开头的为注释行
-    if (!line || line.startsWith('#')) {
-      return;
+    if (evn) {
+        return evn;
     }
 
-    let items = line.split(/\s*=\s*/, 2);
-    evn[items[0]] = parseEvnValue(items[1]);
-  });
+    const chartSet = options.chartSet || 'utf-8';
+    const evnFile = options.filename || '.preprocess-evn';
+    evn = parse(evnFile, chartSet);
 
-  return evn;
+    fs.watchFile(evnFile, () => {
+        evn = parse(evnFile, chartSet);
+    });
+
+    return evn;
+}
+
+function parse(evnFile, chartSet) {
+    const content = fs.readFileSync(evnFile, chartSet).toString();
+    const evn = {};
+
+    content.split(/[\r\n]+/g).map(line => {
+        line = line.trim();
+
+        // 以#开头的为注释行
+        if (!line || line.startsWith('#')) {
+            return;
+        }
+
+        let items = line.split(/\s*=\s*/, 2);
+        evn[items[0]] = parseEvnValue(items[1]);
+    });
+
+    return evn;
 }
 
 module.exports = parseEvn;
